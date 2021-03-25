4cbc7ee69ebda835e104ee2550d5824f6dc1de3f
shivanni <66947306+shivanni@users.noreply.github.com>
2021-03-25 14:11:07 +0530

    Prescriptive report
    done all changes

--- DPMMVCCore/DPM_Testing/wwwroot/DPM/src/app/home/prescriptive/FMEA/prescriptive-report/prescriptive-report.component.ts
+++ DPMMVCCore/DPM_Testing/wwwroot/DPM/src/app/home/prescriptive/FMEA/prescriptive-report/prescriptive-report.component.ts
@@ -1,7 +1,8 @@
 import { DatePipe } from '@angular/common';
 import { ChangeDetectorRef, Component, Input, OnInit } from '@angular/core';
 import html2canvas from 'html2canvas';
-import jspdf from 'jspdf';
+import jspdf, { jsPDF } from 'jspdf';
+
 import { MessageService } from 'primeng/api';
 
 @Component({
@@ -21,7 +22,6 @@ export class PrescriptiveReportComponent implements OnInit {
   public Participants: string = "";
   public prescriptveReportSelect: boolean = true;
   public ReportSelect: boolean = false;
-  public attachmentRemarkTable: boolean = false; 
   public attachmentRemark : any =[]
   public url : string =""
 
@@ -54,64 +54,85 @@ export class PrescriptiveReportComponent implements OnInit {
     await localStorage.removeItem('ReportObj')
   }
 
-  public DownloadPDF(){ 
-   let printContents = document.getElementById('contentToConvert').innerHTML;
-   let originalContents = document.body.innerHTML;
-   document.body.innerHTML = printContents;
-   window.print();
-   document.body.innerHTML = originalContents;
-  // window.print();
-  
-  }
-  
+  // public DownloadPDF(){ 
+  //  let printContents = document.getElementById('contentToConvert').innerHTML;
+  //  let originalContents = document.body.innerHTML;
+  //  document.body.innerHTML = printContents;
+  //  window.print();
+  //  document.body.innerHTML = originalContents;  
+  // }
+
 
   // public DownloadPDF() {
   //   var data = document.getElementById('contentToConvert');
   //   html2canvas(data, { scrollY: -window.scrollY, scale: 1 }).then(canvas => {
-  //     for (var i = 0; i <= data.clientHeight/980; i++) {
   //        var imgWidth = 190;
   //       var pageHeight = 220;
   //       var imgHeight = canvas.height * imgWidth / canvas.width;
   //       var heightLeft = imgHeight;
   //       const contentDataURL = canvas.toDataURL('image/png')
   //       var pdf = new jspdf('p', 'mm', 'a4'); // A4 size page of PDF  
-  //       var position = 2;
-  //       pdf.addImage(contentDataURL, 'PNG', 10, position, imgWidth, imgHeight)
-  //       pdf.setPage(i+1);
-  //     }
-  //     pdf.addPage();
-  //     pdf.save('PrescriptiveFMEA Report.pdf'); // Generated PDF
-  //   });
+  //       var position = 1;
+  //       pdf.addImage(contentDataURL, 'PNG', 10, position, imgWidth, imgHeight) 
+  //        pdf.addPage();
+  //    pdf.save('PrescriptiveFMEA Report.pdf'); // Generated PDF
+  // });
   // } 
 
-  // DownloadPDF() {
-  //   var HTML_Width = 190;
-  //   var HTML_Height = 220;
-  //   var top_left_margin = 15;
-  //   var PDF_Width: number = HTML_Width + (top_left_margin * 2);
-  //   var PDF_Width1: any = HTML_Width + (top_left_margin * 2)
-  //   var PDF_Height = (PDF_Width * 1.5) + (top_left_margin * 2);
-  //   var PDF_Height1: any = (PDF_Width * 1.5) + (top_left_margin * 2);
-  //   var canvas_image_width = HTML_Width;
-  //   var canvas_image_height = HTML_Height;
+  public DownloadPDF() {
+    var data = document.getElementById('contentToConvert');
+    html2canvas(data).then(canvas => {
+    var imgData = canvas.toDataURL('image/png');
+    var imgWidth = 190;
+    var pageHeight = 290;
+    var imgHeight = canvas.height * imgWidth / canvas.width;
+    var heightLeft = imgHeight;
 
-  //   var totalPDFPages = Math.ceil(HTML_Height / PDF_Height) - 1;
+    var doc = new jsPDF('p', 'mm', "a4");
+    var position = 0;
 
-  //   var data = document.getElementById('contentToConvert');
-  //   html2canvas(data, { allowTaint: true }).then(function (canvas) {
-  //     canvas.getContext('2d');
-  //     console.log(canvas.height + "  " + canvas.width);
-  //     var imgData = canvas.toDataURL("image/jpeg", 1.0);
-  //     var pdf = new jspdf('p', 'mm', 'a4',);
-  //     pdf.addImage(imgData, 'JPG', top_left_margin, top_left_margin, canvas_image_width, canvas_image_height);
+    doc.addImage(imgData, 'PNG', 10, position, imgWidth, imgHeight+95);
+    heightLeft -= pageHeight;
+
+    while (heightLeft >= 0) {
+        position = heightLeft - imgHeight;
+        doc.addPage();
+        doc.addImage(imgData, 'PNG', 10, position, imgWidth, imgHeight+95);
+        heightLeft -= pageHeight;
+    }
+  doc.save("PrescriptiveFMEA Report.pdf");
+});
+  }
+
+//  DownloadPDF() {
+//     var HTML_Width = 180;
+//     var HTML_Height = 220;
+//     var top_left_margin = 15;
+//     var PDF_Width: number = HTML_Width + (top_left_margin * 2);
+//     var PDF_Width1: any = HTML_Width + (top_left_margin * 2)
+//     var PDF_Height = (PDF_Width * 1.5) + (top_left_margin * 2);
+//     var PDF_Height1: any = (PDF_Width * 1.5) + (top_left_margin * 2);
+//     var canvas_image_width = HTML_Width;
+//     var canvas_image_height = HTML_Height;
+
+//     var totalPDFPages = Math.ceil(HTML_Height / PDF_Height) +1 ;
+
+//     var data = document.getElementById('contentToConvert');
+//     html2canvas(data, { allowTaint: true }).then(function (canvas) {
+//       canvas.getContext('2d');
+//       console.log(canvas.height + "  " + canvas.width);
+//       var imgData = canvas.toDataURL("image/jpeg", 2.0);
+//       var pdf = new jspdf('p', 'mm', 'a4',);
+//       pdf.addImage(imgData, 'JPG', top_left_margin, top_left_margin, canvas_image_width, canvas_image_height);
   
-  //     for (var i = 1; i <= totalPDFPages; i++) {
-  //       pdf.addPage(PDF_Width1, PDF_Height1);
-  //       pdf.addImage(imgData, 'JPG', top_left_margin, -(PDF_Height * i) + (top_left_margin * 4), canvas_image_width, canvas_image_height); 
-  //     }
-  //     pdf.save("PrescriptiveFMEA Report.pdf");
-  //   });
-  // };
+//       // for (var i = 1; i <= totalPDFPages; i++) {
+//       //   pdf.addPage(PDF_Width1, PDF_Height1);
+//       //   pdf.addImage(imgData, 'JPG', top_left_margin, -(PDF_Height * i) + (top_left_margin * 4), canvas_image_width, canvas_image_height); 
+//       // }
+//       pdf.save("PrescriptiveFMEA Report.pdf");
+//     });
+//   };
+
 
   GeneratePrescriptionReport() {
     if (this.ChairPerson.length > 0 && this.Participants.length > 0) {
