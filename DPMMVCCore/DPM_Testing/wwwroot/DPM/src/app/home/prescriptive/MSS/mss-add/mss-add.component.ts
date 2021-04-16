e52ce13e6937fad81f0a81eb838178f3bdb16810
shivanni <66947306+shivanni@users.noreply.github.com>
2021-04-16 12:19:46 +0530

    MSS Add
    image display  done

--- DPMMVCCore/DPM_Testing/wwwroot/DPM/src/app/home/prescriptive/MSS/mss-add/mss-add.component.ts
+++ DPMMVCCore/DPM_Testing/wwwroot/DPM/src/app/home/prescriptive/MSS/mss-add/mss-add.component.ts
@@ -9,7 +9,7 @@ import { CommonLoadingDirective } from 'src/app/shared/Loading/common-loading.di
   selector: 'app-mss-add',
   templateUrl: './mss-add.component.html',
   styleUrls: ['./mss-add.component.scss'],
-  providers: [MessageService],
+  providers: [MessageService]
 })
 export class MSSAddComponent implements OnInit {
 
@@ -67,9 +67,8 @@ export class MSSAddComponent implements OnInit {
     this.title.setTitle('DPM | MSS');
     var MSSData = JSON.parse(localStorage.getItem('MSSObject'))
      this.data1 = JSON.parse(MSSData.FMWithConsequenceTree)
-    //  this.MSSTree = this.data1[0].children[0].children[0].children
       this.MSSTree = this.data1[0].children[0].children[0].Consequence[0].children[0].children[0].children[0].children[2].data.name
-  this.ShowConsequence()
+      this.ShowConsequence()
     }
 
   async ngOnDestroy() {
@@ -114,7 +113,7 @@ export class MSSAddComponent implements OnInit {
    if(this.AOCM.length >0 || this.ASO.length >0 || this.ASR.length >0 || this.AFFT.length >0 || this.AFromAbove.length >0 || this.ARED.length >0 ){
     this.changeDetectorRef.detectChanges()
    }else{
-    this.messageService.add({ severity: 'warn', summary: 'warn', detail: "fill the data" })
+    alert("fill the data")
    }
   }
   ConsequenceSecond(){
@@ -123,35 +122,30 @@ export class MSSAddComponent implements OnInit {
       this.changeDetectorRef.detectChanges()
     }else{
       alert("fill the data")
-     this.messageService.add({ severity: 'warn', summary: 'warn', detail: "fill the data" })
     }
-
   }
   ConsequenceThird(){
 
     if(this.COCM.length >0 || this.CSO.length >0 || this.CSR.length >0 || this.COFM.length >0 ||  this.CRED.length >0 ){
       this.changeDetectorRef.detectChanges()
     }else{
-     this.messageService.add({ severity: 'warn', summary: 'warn', detail: "fill the data" })
+      alert("fill the data")
     }
 
   }
   ConsequenceFourth(){
-
-  
     if(this.DOCM.length >0 || this.DSO.length >0 || this.DSR.length >0 || this.DOFM.length >0 || this.DRED.length >0 ){
       this.changeDetectorRef.detectChanges()
     }else{
-     this.messageService.add({ severity: 'warn', summary: 'warn', detail: "fill the data" })
+      alert("fill the data")
     }
 
   }
   ConsequenceFive(){
- 
     if(this.EOCM.length >0 || this.ESO.length >0 || this.ESR.length >0 || this.EFFT.length >0 || this.EOFM.length >0 || this.ERED.length >0 ){
       this.changeDetectorRef.detectChanges()
     }else{
-     this.messageService.add({ severity: 'warn', summary: 'warn', detail: "fill the data" })
+      alert("fill the data")
     }
 
   }
