8bc8f98d8deaa87bf72a58d5e8bb43e63c0d7d28
shivanni <66947306+shivanni@users.noreply.github.com>
2021-05-19 14:13:41 +0530

    Panzoom

--- DPMMVCCore/DPM_Testing/wwwroot/DPM/src/app/home/prescriptive/RCA/rca.component.ts
+++ DPMMVCCore/DPM_Testing/wwwroot/DPM/src/app/home/prescriptive/RCA/rca.component.ts
@@ -34,9 +34,6 @@ export class RCAComponent implements OnInit, AfterViewInit {
     @ViewChild('scene',{ static: false }) scene: ElementRef;
     @ViewChild('scene1',{ static: false }) scene1: ElementRef;
     panZoomController;
-
-    public currentZoomLevel: number;
-    zoomLevels: number[];
     files: any = [];
     Updatefiles: any[];
     selectedFile: any;
@@ -63,15 +60,14 @@ export class RCAComponent implements OnInit, AfterViewInit {
     public RCADisplayLabel: string = ""
     public RCADisplayFile: any = []
     public tabView: boolean = true
-    
-
+    pinchSpeed
+    elementYouWant: any;
     constructor(private messageService: MessageService,
         public commonLoadingDirective: CommonLoadingDirective,
         private changeDetectorRef: ChangeDetectorRef,
         public router: Router,
         private commonBL: CommonBLService,
         private RCAAPIName: PrescriptiveContantAPI,
-        private elRef:ElementRef,
          ) {
         this.files = [{
             id: this.itemCount,
@@ -92,60 +88,18 @@ export class RCAComponent implements OnInit, AfterViewInit {
                 children: []
             }
         )
-
+  
     }
     ngOnInit() {
         this.getRecordsList();
         this.getHeatExchangerData();
     }
-
-    zoom() {
-        const isSmooth = false;
-        const scale = this.currentZoomLevel;
-        if (scale) {
-          const transform = this.panZoomController.getTransform();
-          const deltaX = transform.x;
-          const deltaY = transform.y;
-          const offsetX = scale + deltaX;
-          const offsetY = scale + deltaY;
-    
-          if (isSmooth) {
-            this.panZoomController.smoothZoom(0, 0, scale);
-          } else {
-            this.panZoomController.zoomTo(offsetX, offsetY, scale);
-          }
-        }
-      }
-      
-    
-      zoomToggle(zoomIn: boolean) {
-        const idx = this.zoomLevels.indexOf(this.currentZoomLevel);
-        if (zoomIn) {
-          if (typeof this.zoomLevels[idx + 1] !== 'undefined') {
-            this.currentZoomLevel = this.zoomLevels[idx + 1];
-          }
-        } else {
-          if (typeof this.zoomLevels[idx - 1] !== 'undefined') {
-            this.currentZoomLevel = this.zoomLevels[idx - 1];
-          }
-        }
-        if (this.currentZoomLevel === 1) {
-          this.panZoomController.moveTo(0, 0);
-          this.panZoomController.zoomAbs(0, 0, 1);
-        } else {
-          this.zoom();
-        }
-      }
-    
       ngAfterViewInit() {
-        this.zoomLevels = [0.1, 0.25, 0.5, 0.75, 1, 1.25, 1.5, 1.75, 2, 2.5, 3];
-        this.currentZoomLevel = this.zoomLevels[6];
-        // panzoom(document.querySelector('#scene'));
          this.panZoomController = panzoom(this.scene.nativeElement);
          this.panZoomController = panzoom(this.scene1.nativeElement);
+         this.pinchSpeed = -1
       }
-
-
+      
     getHeatExchangerData() {
         const params = new HttpParams()
             .set("data", 'Heat Exchanger')
