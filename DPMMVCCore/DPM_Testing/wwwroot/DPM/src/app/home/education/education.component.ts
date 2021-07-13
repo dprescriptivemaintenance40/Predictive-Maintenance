dfc7eab4da4b05fe9c5e4ab7f85ef4c4c2c9fe82
shivanni <66947306+shivanni@users.noreply.github.com>
2021-07-13 20:17:06 +0530

    done

--- DPMMVCCore/DPM_Testing/wwwroot/DPM/src/app/home/education/education.component.ts
+++ DPMMVCCore/DPM_Testing/wwwroot/DPM/src/app/home/education/education.component.ts
@@ -15,6 +15,7 @@ export class EducationComponent {
   }
   ngOnInit() {
     this.showReport()
+    this.showReport1()
   }
   article1() {
     let link = document.createElement("a");
@@ -29,6 +30,10 @@ export class EducationComponent {
     let embedReportId = '8229f0b7-523d-46d9-9a54-b53438061991';
     let reportContainer = <HTMLElement>document.getElementById('reportContainer');
     let powerbi = new pbi.service.Service(pbi.factories.hpmFactory, pbi.factories.wpmpFactory, pbi.factories.routerFactory);
- 
+  }
+
+  
+  showReport1() {
+    let embedUrl = 'https://app.powerbi.com/reportEmbed?reportId=2c517729-b2cc-413b-9b52-c641d53085e3&autoAuth=true&ctid=606acdf9-2783-4b1f-9afc-a0919c38927d&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLXdlc3QtZXVyb3BlLWUtcHJpbWFyeS1yZWRpcmVjdC5hbmFseXNpcy53aW5kb3dzLm5ldC8ifQ%3D%3D';
 }
 }
