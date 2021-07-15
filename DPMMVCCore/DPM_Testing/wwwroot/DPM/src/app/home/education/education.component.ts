393eb480f228bcc7c9adbef425c4d1120b64336d
shivanni <66947306+shivanni@users.noreply.github.com>
2021-07-15 14:27:47 +0530

    done

--- DPMMVCCore/DPM_Testing/wwwroot/DPM/src/app/home/education/education.component.ts
+++ DPMMVCCore/DPM_Testing/wwwroot/DPM/src/app/home/education/education.component.ts
@@ -15,7 +15,7 @@ export class EducationComponent {
   }
   ngOnInit() {
     this.showReport()
-    this.showReport1()
+    // this.showReport1()
   }
   article1() {
     let link = document.createElement("a");
@@ -33,7 +33,7 @@ export class EducationComponent {
   }
 
   
-  showReport1() {
-    let embedUrl = 'https://app.powerbi.com/reportEmbed?reportId=2c517729-b2cc-413b-9b52-c641d53085e3&autoAuth=true&ctid=606acdf9-2783-4b1f-9afc-a0919c38927d&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLXdlc3QtZXVyb3BlLWUtcHJpbWFyeS1yZWRpcmVjdC5hbmFseXNpcy53aW5kb3dzLm5ldC8ifQ%3D%3D';
-}
+//   showReport1() {
+//     let embedUrl = 'https://app.powerbi.com/reportEmbed?reportId=2c517729-b2cc-413b-9b52-c641d53085e3&autoAuth=true&ctid=606acdf9-2783-4b1f-9afc-a0919c38927d&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLXdlc3QtZXVyb3BlLWUtcHJpbWFyeS1yZWRpcmVjdC5hbmFseXNpcy53aW5kb3dzLm5ldC8ifQ%3D%3D';
+// }
 }
